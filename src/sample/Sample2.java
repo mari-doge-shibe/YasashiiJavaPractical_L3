8ea883c790a05f04eaff63296a3090dc1939e2a7
mari_s <ms.mari.suzuki@gmail.com>
2020-06-21 21:22:57 +0900

    Sample 2 created (with a button)

--- src/sample/Sample2.java
+++ src/sample/Sample2.java
@@ -1,5 +1,34 @@
 package sample;
 
-public class Sample2 {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Sample2 extends Application {
+    private Button bt;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        bt = new Button();
+
+        bt.setText("購入");
+
+        BorderPane bp = new BorderPane();
+
+        bp.setCenter(bt);
+
+        // create scene
+        Scene sc = new Scene(bp, 300, 200);
+
+        stage.setScene(sc);
+
+        stage.setTitle("サンプル");
+        stage.show();
+    }
 
 }
