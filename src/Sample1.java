013331ebdd0aad8961a5fb195b6b7180a14c517b
mari_s <ms.mari.suzuki@gmail.com>
2020-06-21 21:14:47 +0900

    Initial commit

---
+++ src/Sample1.java
@@ -0,0 +1,32 @@
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class Sample1 extends Application {
+
+    private Label lb;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        lb = new Label();
+
+        lb.setText("いらっしゃいませ");
+
+        BorderPane bp = new BorderPane();
+
+        bp.setCenter(lb);
+
+        Scene sc = new Scene(bp, 300, 200);
+
+        stage.setScene(sc);
+
+        stage.setTitle("サンプル");
+        stage.show();
+    }
+
+}
